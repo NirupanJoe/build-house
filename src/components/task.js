7af9c877f7d902ed227ba3be983e04a9b4ddc116
Nirupan Joe <nirupanjoej@gmail.com>
2021-07-19 10:29:52 +0000

    change some style.

--- src/components/task.js
+++ src/components/task.js
@@ -1,9 +1,11 @@
 import { React } from 'react';
 
 const Task = ({ tasks, name, totalCost }) =>
-	<div className="task">
-		<div className="name">{name}</div>
-		<div className="total-cost">{totalCost}</div>
+	<div className="task-title">
+		<div className="task">
+			<div className="name">{name}</div>
+			<div className="total-cost">{totalCost}</div>
+		</div>
 		<div>{tasks.map(Task)}</div>
 	</div>;
 
